397baa041f1190a6c982170176b3c1ff344f5c85
Ozoku <19756856+ozokuz@users.noreply.github.com>
2024-08-30 14:56:24 +0300

    more programming stuff

--- home/base/shell/programming.nix
+++ home/base/shell/programming.nix
@@ -1,20 +1,32 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    nodejs_20
+{ pkgs, pkgs-unstable, ... }: {
+  home.packages = with pkgs-unstable; [
+    nodejs_22
     bun
     deno
-    go
+    pkgs.go
     opam
     rustup
     poetry
     python312
+    elixir_1_17
+    dotnet-sdk_8
+    ruby_3_3
+    swift
+    swiftPackages.swiftpm
+    gleam
+    nim
+    nimble
+    nim-atlas
+    odin
+    haxe
+    zig
+    ghc
+    crystal
+    dart
     # lua-language-server
     # clang-tools_17
     # marksman
     # stylua
-    elixir_1_16
-    ruby_3_3
-    dotnet-sdk_7
   ];
 
   xdg.configFile."npm/npmrc".source = ./npmrc;
