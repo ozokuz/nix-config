9549b43dd1c15a01de679b342d8866e1a04314a5
Ozoku <19756856+ozokuz@users.noreply.github.com>
2024-10-24 15:47:58 +0300

    desktop modules

---
+++ modules/nixos/desktop/default.nix
@@ -0,0 +1,11 @@
+{
+  base = import ./base.nix;
+  audio = import ./audio.nix;
+  connectivity = import ./connectivity.nix;
+  display-manager = import ./display-manager.nix;
+  fonts = import ./fonts.nix;
+  gaming = import ./gaming.nix;
+  hyprland = import ./hyprland.nix;
+  packaging = import ./packaging.nix;
+  secrets = import ./secrets.nix;
+}
