cdb2e343a1393dbfc249e46a44c2904129c92d75
Ozoku <19756856+ozokuz@users.noreply.github.com>
2024-10-29 13:23:36 +0200

    libvirt tpm support

--- hosts/common/optional/virtualization/libvirt.nix
+++ hosts/common/optional/virtualization/libvirt.nix
@@ -1,4 +1,16 @@
-{
-  virtualisation.libvirtd.enable = true;
+{pkgs, ...}:{
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd];
+      };
+    };
+  };
   programs.virt-manager.enable = true;
 }
