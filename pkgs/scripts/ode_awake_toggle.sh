5cae66d863f7b13a97731dda5270ccb24e04f84e
Ozoku <19756856+ozokuz@users.noreply.github.com>
2024-10-31 01:17:40 +0200

    only block idle detection

--- pkgs/scripts/ode_awake_toggle.sh
+++ pkgs/scripts/ode_awake_toggle.sh
@@ -4,7 +4,7 @@ if pgrep -x ozokuidle; then
   killall ozokuidle
 else
   # shellcheck disable=SC2016
-  systemd-inhibit --who=ozokuidle --why="Prevent idling" --mode=block perl -MPOSIX -e '$0="ozokuidle"; pause' &
+  systemd-inhibit --who=ozokuidle --why="Prevent idling" --what=idle --mode=block perl -MPOSIX -e '$0="ozokuidle"; pause' &
 fi
 
 pkill -SIGRTMIN+8 waybar
