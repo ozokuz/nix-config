a48c8cae5236faa6095386f67ebd2bd082e2f8e0
Ozoku <19756856+ozokuz@users.noreply.github.com>
2024-10-30 17:34:02 +0200

    remove xwaylandvideobridge fix

--- home/ozoku/features/desktop/hyprland/windowrules.nix
+++ home/ozoku/features/desktop/hyprland/windowrules.nix
@@ -1,14 +1,6 @@
 {...}: {
   wayland.windowManager.hyprland.settings = {
     windowrulev2 = [
-      # XWaylandVideoBridge
-      "opacity 0.0 override 0.0 override,class:^(xwaylandvideobridge)$"
-      "noanim,class:^(xwaylandvideobridge)$"
-      "nofocus,class:^(xwaylandvideobridge)$"
-      "noinitialfocus,class:^(xwaylandvideobridge)$"
-
-      # Don't Focus Emoji Picker
-
       # Tearing
       "immediate, class:^(Golf With Your Friends.x86_64)$"
       "immediate, class:^(overwatch.exe)$"
