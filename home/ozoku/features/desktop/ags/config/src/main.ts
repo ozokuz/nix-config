02e76f1b82d93df32773a189fbb1b06005afc13e
Ozoku <19756856+ozokuz@users.noreply.github.com>
2024-11-07 09:28:35 +0200

    start of configuring ags

--- home/ozoku/features/desktop/ags/config/src/main.ts
+++ home/ozoku/features/desktop/ags/config/src/main.ts
@@ -1,9 +1,20 @@
 const Bar = (monitor: number) =>
   Widget.Window({
     name: `bar-${monitor}`,
+    anchor: ["top", "left", "right"],
+    margins: [8, 0, 0, 0],
+    exclusivity: "exclusive",
     child: Widget.Label("hello"),
   });
 
+const cssFile = "./style.css";
+
 App.config({
   windows: [Bar(0)],
+  style: cssFile,
+});
+
+Utils.monitorFile(cssFile, () => {
+  App.resetCss();
+  App.applyCss(cssFile);
 });
