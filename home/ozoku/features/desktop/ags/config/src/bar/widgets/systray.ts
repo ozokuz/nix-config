8956ab420316be49f7b70708d3ea8dfeed1bd8a2
Ozoku <19756856+ozokuz@users.noreply.github.com>
2024-11-09 17:46:44 +0200

    filtering for systray items

--- home/ozoku/features/desktop/ags/config/src/bar/widgets/systray.ts
+++ home/ozoku/features/desktop/ags/config/src/bar/widgets/systray.ts
@@ -1,16 +1,30 @@
 const systemtray = await Service.import("systemtray");
 
+// const excluded = ["spotify-client", "gammastep", "nm-applet", "Bitwarden"];
+const excluded = [] as string[];
+
 export const SysTray = () =>
   Widget.Box({
     className: "tray",
     children: systemtray.bind("items").as((items) =>
-      items.map((item) =>
-        Widget.Button({
-          child: Widget.Icon({ icon: item.bind("icon") }),
-          onPrimaryClick: (_, event) => item.activate(event),
-          onSecondaryClick: (_, event) => item.openMenu(event),
-          tooltip_markup: item.bind("tooltip_markup"),
-        })
-      )
+      items
+        .filter(
+          (i) =>
+            !excluded.includes(
+              i.id === "chrome_status_icon_1" ? i.tooltip_markup : i.id
+            )
+        )
+        .map((item) =>
+          Widget.Button({
+            child: Widget.Icon({ icon: item.bind("icon") }),
+            onPrimaryClick: (_, event) => item.activate(event),
+            onSecondaryClick: (_, event) => item.openMenu(event),
+            tooltip_markup: item.bind("tooltip_markup"),
+          })
+        )
     ),
   });
+
+// Utils.timeout(1000, () => {
+//   console.log(systemtray.items);
+// });
