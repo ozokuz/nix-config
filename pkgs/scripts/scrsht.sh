22bde148e6a6bfd709d273803506cc02f6faed5b
Ozoku <19756856+ozokuz@users.noreply.github.com>
2024-11-09 17:55:42 +0200

    maybe fix monitor screenshots

--- pkgs/scripts/scrsht.sh
+++ pkgs/scripts/scrsht.sh
@@ -6,41 +6,41 @@ screenshot_area=""
 
 case $1 in
 area)
-	screenshot_area=$(slurp)
-	;;
+  screenshot_area=$(slurp)
+  ;;
 
 window)
-	json=$(hyprctl activewindow -j)
-	wx=$(echo "$json" | jq '.at[0]' --raw-output)
-	wy=$(echo "$json" | jq '.at[1]' --raw-output)
-	ww=$(echo "$json" | jq '.size[0]' --raw-output)
-	wh=$(echo "$json" | jq '.size[1]' --raw-output)
-	screenshot_area="${wx},${wy} ${ww}x${wh}"
-	;;
+  json=$(hyprctl activewindow -j)
+  wx=$(echo "$json" | jq '.at[0]' --raw-output)
+  wy=$(echo "$json" | jq '.at[1]' --raw-output)
+  ww=$(echo "$json" | jq '.size[0]' --raw-output)
+  wh=$(echo "$json" | jq '.size[1]' --raw-output)
+  screenshot_area="${wx},${wy} ${ww}x${wh}"
+  ;;
 
 screen)
-	json=$(hyprctl monitors -j)
-	sx=$(echo "$json" | jq -r '.[] | select(.focused) | .x' --raw-output)
-	sy=$(echo "$json" | jq -r '.[] | select(.focused) | .y' --raw-output)
-	sw=$(echo "$json" | jq -r '.[] | select(.focused) | .width' --raw-output)
-	sh=$(echo "$json" | jq -r '.[] | select(.focused) | .height' --raw-output)
-	screenshot_area="${sx},${sy} ${sw}x${sh}"
-	;;
+  json=$(hyprctl monitors -j)
+  sx=$(echo "$json" | jq -r '.[] | select(.focused) | .x' --raw-output)
+  sy=$(echo "$json" | jq -r '.[] | select(.focused) | .y' --raw-output)
+  sw=$(echo "$json" | jq -r '.[] | select(.focused) | .width/.scale' --raw-output)
+  sh=$(echo "$json" | jq -r '.[] | select(.focused) | .height/.scale' --raw-output)
+  screenshot_area="${sx},${sy} ${sw}x${sh}"
+  ;;
 
 *)
-	echo Invalid argument
-	exit 1
-	;;
+  echo Invalid argument
+  exit 1
+  ;;
 esac
 
 if ! grim -g "$screenshot_area" "$screenshot_path"; then
-	notify-send Screenshot "Canceled"
-	exit 1
+  notify-send Screenshot "Canceled"
+  exit 1
 fi
 
 if ! json=$(curl -X POST -F "file=@$screenshot_path" "$url" -o-); then
-	notify-send Screenshot "Upload Failed"
-	exit 1
+  notify-send Screenshot "Upload Failed"
+  exit 1
 fi
 
 echo "$json" | jq '.attachments[0].url' --raw-output | awk -F? '{print $1}' | wl-copy -n
