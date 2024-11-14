35ae78490819aad566283fcb291f45658d4ae33c
Ozoku <19756856+ozokuz@users.noreply.github.com>
2024-11-14 21:37:30 +0200

    some prompt changes

--- home/ozoku/features/cli/prompt.nix
+++ home/ozoku/features/cli/prompt.nix
@@ -4,49 +4,68 @@
     settings = {
       add_newline = true;
       format = lib.concatStrings [
+        "[](color_indigo)"
         "$username"
+        "[@](bold bg:color_indigo fg:color_fg0)"
         "$hostname"
+        "[](fg:color_indigo bg:color_bg0)"
         "$git_branch"
         "$git_status"
+        "[](fg:color_bg0 bg:color_purple)"
         "$directory"
+        "[ ](bg:color_purple)[](color_purple) "
       ];
+
+      palette = "ozoku";
+
+      palettes.ozoku = {
+        color_bg0 = "#303040";
+        color_fg0 = "#c0caf5";
+        color_fg1 = "#cad3f5";
+        color_indigo = "#5c73e6";
+        color_lime = "#29d916";
+        color_purple = "#cc1fbd";
+        color_purple2 = "b324a7";
+        color_green = "#40a02b";
+        color_red = "#e64553";
+      };
 
       username = {
         show_always = true;
-        format = "[](#5c73e6)[$user ]($style)";
-        style_user = "bold bg:#5c73e6 #29d916";
+        format = "[$user]($style)";
+        style_user = "bold bg:color_indigo fg:color_lime";
       };
 
       hostname = {
         ssh_only = false;
-        format = "[@](bold bg:#5c73e6 #cad3f5)[ $hostname ]($style)[](#5c73e6 bg:#24283B)";
-        style = "bold bg:#5c73e6 #cc1fbd";
+        format = "[$hostname ]($style)";
+        style = "bold bg:color_indigo fg:color_purple";
       };
 
       git_branch = {
         format = "[ $symbol$branch ]($style)";
-        style = "bold bg:#24283B #40a02b";
+        style = "bold bg:color_bg0 fg:color_green";
       };
 
       git_status = {
         format = "[\\[$all_status$ahead_behind\\] ]($style)";
-        conflicted = "C";
+        conflicted = "?";
         ahead = "^";
         behind = "v";
         diverged = "-";
-        up_to_date = " ";
+        up_to_date = "\\$";
         untracked = "U";
         stashed = "S";
         modified = "M";
         staged = "+";
-        renamed = ">";
-        deleted = "x";
-        style = "bold bg:#24283B #e64553";
+        renamed = "R";
+        deleted = "D";
+        style = "bold bg:color_bg0 fg:color_red";
       };
 
       directory = {
-        format = "[](#24283B bg:#b324a7)[ $path]($style)[$read_only]($read_only_style)[ ](bg:#b324a7)[](#b324a7) ";
-        style = "bold bg:#b324a7";
+        format = "[ $path]($style)[$read_only]($read_only_style)";
+        style = "bold bg:color_purple fg:color_fg0";
         fish_style_pwd_dir_length = 1;
       };
     };
