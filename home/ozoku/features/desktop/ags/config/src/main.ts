04beb0d4d7d91f3a0b62dfbf7bb59141d0408cf1
Ozoku <19756856+ozokuz@users.noreply.github.com>
2024-11-15 01:23:22 +0200

    trying to get bars to correctly show up
    when connecting monitors

--- home/ozoku/features/desktop/ags/config/src/main.ts
+++ home/ozoku/features/desktop/ags/config/src/main.ts
@@ -1,9 +1,27 @@
+import Gtk from "types/@girs/gtk-3.0/gtk-3.0";
 import { Bar } from "./bar/bar";
 import { Widgets } from "./widgets";
+import Gdk from "gi://Gdk";
+
+const hyprland = await Service.import("hyprland");
+
+function forMonitors(widget: (monitor: number) => Gtk.Window) {
+  const n = Gdk.Display.get_default()?.get_n_monitors() ?? 1;
+  return Array.from({ length: n }, (_, i) => widget(i));
+}
 
 App.config({
-  windows: [Bar(0), ...Widgets],
+  windows: () => [...forMonitors(Bar), ...Widgets],
   style: "./src/style.css",
+});
+
+hyprland.connect("monitor-added", (_, name: string) => {
+  Utils.timeout(100, () => {
+    const monitor = hyprland.monitors.find((m) => m.name === name)?.id;
+    if (!monitor) return;
+
+    App.addWindow(Bar(monitor));
+  });
 });
 
 Utils.timeout(100, () =>
