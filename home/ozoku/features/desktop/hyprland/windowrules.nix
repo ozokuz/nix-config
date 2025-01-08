25666f1bb34df75fb87b421392d8d5eaa78d3b14
Ozoku <19756856+ozokuz@users.noreply.github.com>
2025-01-08 15:53:05 +0200

    hearthstone todo

--- home/ozoku/features/desktop/hyprland/windowrules.nix
+++ home/ozoku/features/desktop/hyprland/windowrules.nix
@@ -4,6 +4,16 @@
       # Emote
       "noinitialfocus, class:^(emote)$"
 
+      # Hearthstone
+      "float, class:(hearthstone deck tracker.exe)"
+      "center, class:(hearthstone deck tracker.exe)"
+      "nofocus, class:(hearthstone deck tracker.exe)"
+      "noinitialfocus, class:(hearthstone deck tracker.exe)"
+      "noborder, class:(hearthstone deck tracker.exe)"
+      "pin, class:(hearthstone deck tracker.exe)"
+      "opacity 0.2 0.1, class:(hearthstone deck tracker.exe)"
+      "stayfocused, class:(hearthstone.exe)"
+
       # Tearing
       "immediate, class:^(Golf With Your Friends.x86_64)$"
       "immediate, class:^(overwatch.exe)$"
