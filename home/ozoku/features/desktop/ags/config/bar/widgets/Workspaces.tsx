6ee381a9aa047dd015dcb736359aff398d557cc9
Ozoku <19756856+ozokuz@users.noreply.github.com>
2025-03-21 09:55:00 +0200

    ags: fix workspace buttons showing wrong state
    when moving windows between workspaces

--- home/ozoku/features/desktop/ags/config/bar/widgets/Workspaces.tsx
+++ home/ozoku/features/desktop/ags/config/bar/widgets/Workspaces.tsx
@@ -2,47 +2,59 @@ import { bind, Binding, Variable } from "astal";
 import Hyprland from "gi://AstalHyprland";
 import style from "./Workspaces.scss";
 import { App } from "astal/gtk4";
+import { ButtonProps } from "astal/gtk4/widget";
+import { range } from "../../utils";
 
-export default function Workspaces({ monitor }: { monitor: Binding<number> }) {
-  App.apply_css(style);
-  const hyprland = Hyprland.get_default();
-  const monitorWorkspacesData = Variable.derive(
+type WorkspaceButtonProps = ButtonProps & {
+  ws: Hyprland.Workspace;
+  num: number;
+};
+
+const hyprland = Hyprland.get_default();
+
+const moveCount = Variable(0);
+hyprland.connect("client-moved", () => {
+  moveCount.set(moveCount.get() + 1);
+});
+
+function WorkspaceButton({ ws, num, ...props }: WorkspaceButtonProps) {
+  const classes = Variable.derive(
     [
-      monitor,
-      bind(hyprland, "workspaces"),
-      bind(hyprland, "monitors"),
       bind(hyprland, "focusedWorkspace"),
+      bind(hyprland, "clients"),
+      moveCount(),
     ],
-    (monitor, workspaces, monitors, focusedWorkspace) => ({
-      monitor,
-      workspaces,
-      monitors,
-      focusedWorkspace,
-    })
+    (fws) =>
+      [
+        "base",
+        fws?.id === ws.id ? "focused" : "",
+        hyprland.get_workspace(ws.id)?.get_clients()?.length > 0 ? "" : "empty",
+      ].filter(Boolean)
   );
 
   return (
-    <box cssClasses={["workspaces"]}>
-      {monitorWorkspacesData().as((wsd) =>
-        Array.from({ length: 9 }).map((_, n) => {
-          const wsnum = wsd.monitor * 10 + n + 1;
-          const ws = hyprland.get_workspace(wsnum);
+    <button
+      cssClasses={classes()}
+      onDestroy={() => classes.drop()}
+      onClicked={() => ws.focus()}
+      {...props}
+    >
+      {num}
+    </button>
+  );
+}
 
-          return (
-            <button
-              cssClasses={[
-                "base",
-                !ws || ws.clients.length === 0 ? "empty" : "",
-                wsd.focusedWorkspace === ws ? "focused" : "",
-              ].filter(Boolean)}
-              onClicked={() =>
-                hyprland.message_async(`dispatch workspace ${wsnum}`, () => {})
-              }
-            >
-              {n + 1}
-            </button>
-          );
-        })
+export default function Workspaces({ monitor }: { monitor: Binding<number> }) {
+  App.apply_css(style);
+  return (
+    <box cssClasses={["workspaces"]}>
+      {monitor.as((m) =>
+        range(9, 1).map((i) => (
+          <WorkspaceButton
+            ws={Hyprland.Workspace.dummy(m * 10 + i, null)}
+            num={i}
+          />
+        ))
       )}
     </box>
   );
