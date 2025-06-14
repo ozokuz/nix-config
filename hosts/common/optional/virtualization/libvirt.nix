887f5ffa1fa9399f56bc33a7d55afd7f04302c47
Ozoku <19756856+ozokuz@users.noreply.github.com>
2025-06-14 12:22:21 +0300

    more virtualization stuff

--- hosts/common/optional/virtualization/libvirt.nix
+++ hosts/common/optional/virtualization/libvirt.nix
@@ -1,4 +1,14 @@
 {pkgs, ...}:{
+  boot.extraModprobeConfig = ''
+    options kvm_intel nested=1
+    options kvm_intel emulate_invalid_guest_state=0
+    options kvm ignore_msrs=1 report_ignored_msrs=0
+  '';
+  environment.systemPackages = with pkgs; [
+    dmg2img
+    libguestfs
+    p7zip
+  ];
   virtualisation.libvirtd = {
     enable = true;
     qemu = {
--- hosts/common/users/ozoku/default.nix
+++ hosts/common/users/ozoku/default.nix
@@ -11,6 +11,7 @@
       "input"
       "video"
       "storage"
+      "kvm"
     ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN9iloUdc5cvzURayOT4rBhsAh/5fBTrVfv7sVMtG8i6 ozoku@cosmos"
